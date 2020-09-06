4a0d2ea67539fa0869b34916e9e2dad0b376fb1a
RyotaKanemoto <r618-black.s@outlook.jp>
2020-09-06 17:57:38 +0900

    p66

--- app/app/Http/Controllers/HelloController.php
+++ app/app/Http/Controllers/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 global $body;
 
@@ -10,23 +11,30 @@ $body = "global body";
 
 class HelloController extends Controller
 {
-    public function index($id = 'noname', $pass = 'nopass') {
-        return <<<EOF
-<html>
-    <body>
-        hello
-        <ul>
-            <li>ID: {$id}</li>
-            <li>pass: {$pass}</li>
-            <li><a = href="hello/other">other</a></li>
-        </ul>
-    </body>
-</html>
-EOF;
-    }
-
-    public function other() {
-        global $body;
-        return "<html><body>" . $body . "</body></html>";
+//     public function index(Request $request, Response $response) {
+//         return <<<EOF
+// <html>
+//     <body>
+//         hello
+//         <ul>
+//             <li>request: <pre>{$request}</pre></li>
+//             <li>response: <pre>{$response}</pre></li>
+//         </ul>
+//     </body>
+// </html>
+// EOF;
+//     }
+//
+//     public function other() {
+//         global $body;
+//         return "<html><body>" . $body . "</body></html>";
+//     }
+    public function index($id = 'zero')
+    {
+        $data = [
+            'msg' => 'test_message',
+            'id' => $id
+        ];
+        return view('hello.index', $data);
     }
 }
