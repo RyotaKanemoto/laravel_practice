1e06176f83c0c087c04e97d04f29429f43c99432
RyotaKanemoto <r618-black.s@outlook.jp>
2020-09-20 23:05:20 +0900

    p90

--- app/resources/views/hello/index.blade.php
+++ app/resources/views/hello/index.blade.php
@@ -1,25 +1,17 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-    <style>
-    body {
-        font-size: 16pt;
-        color: #999;
-    }
-    h1 {
-        font-size: 100pt;
-        text-align: right;
-        color: #f6f6f6;
-        margin: -50px 0px -100px 0px;
-    }
-    </style>
-</head>
-<body>
-    <h1>Index</h1>
-    <p>{{$msg}}</p>
-    <form method="POST" action="/hello">
-        @csrf
-        <input type="text" name="msg">
-        <input type="submit">
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'IndexTest')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+kaneryo
+@endsection
