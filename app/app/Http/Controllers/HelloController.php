515cbaff7a50b0da06ec0c4cbd3fa49724a79dde
RyotaKanemoto <kanemoto.dev@gmail.com>
2021-01-10 15:04:23 +0900

    p96 include each

--- app/app/Http/Controllers/HelloController.php
+++ app/app/Http/Controllers/HelloController.php
@@ -11,30 +11,14 @@ $body = "global body";
 
 class HelloController extends Controller
 {
-//     public function index(Request $request, Response $response) {
-//         return <<<EOF
-// <html>
-//     <body>
-//         hello
-//         <ul>
-//             <li>request: <pre>{$request}</pre></li>
-//             <li>response: <pre>{$response}</pre></li>
-//         </ul>
-//     </body>
-// </html>
-// EOF;
-//     }
-//
-//     public function other() {
-//         global $body;
-//         return "<html><body>" . $body . "</body></html>";
-//     }
     public function index(Request $request)
     {
         $data = [
-            'msg' => 'お名前を入力してください',
+            ['name' => 'yamada', 'mail' => 'yamada@'],
+            ['name' => 'tanaka', 'mail' => 'tanaka@'],
+            ['name' => 'suzuki', 'mail' => 'suzuki@'],
         ];
-        return view('hello.index', $data);
+        return view('hello.index', ['data' => $data]);
     }
 
     public function post(Request $request)
